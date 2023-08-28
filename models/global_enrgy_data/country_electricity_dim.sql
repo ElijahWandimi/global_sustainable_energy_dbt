4340312a0b856aebcae5e01b8a31d194ad5b818c
Elijah Wandimi <wandimiem7@gmail.com>
2023-08-28 01:06:46 +0000

    add table views

---
+++ models/global_enrgy_data/country_electricity_dim.sql
@@ -0,0 +1,29 @@
+with electricity_dim as (
+    select  electricity_data_id,
+            Electricity_from_fossil_fuels,
+            Electricity_from_nuclear,
+            Electricity_from_renewables,
+            `Low-carbon_electricity_perct` AS Low_carbon_electricity_perct,
+            Access_to_electricity_perct,
+    from `sonorous-asset-391616.global_sustainable_energy_data.electricity_dim`
+),
+country_dim AS (
+    SELECT *
+    FROM `sonorous-asset-391616.global_sustainable_energy_data.country_dim`
+),
+final AS (
+    SELECT  e.electricity_data_id,
+            e.Electricity_from_fossil_fuels,
+            e.Electricity_from_nuclear,
+            e.Electricity_from_renewables,
+            e.Low_carbon_electricity_perct,
+            e.Access_to_electricity_perct,
+            c.Entity,
+            c.Land_Area,
+    FROM electricity_dim e
+    INNER JOIN country_dim c
+    ON e.electricity_data_id = c.country_id
+    WHERE electricity_data_id IS NOT NULL
+)
+
+SELECT * FROM final
